1c4470ab100e968e1b31d5f9ec71aa93f9343b31
Krasavceva <krasavceva.va@gmail.com>
2023-05-01 21:02:19 +0300

    feat: add arrays keyboard

---
+++ index.js
@@ -0,0 +1,9 @@
+// массивы символов
+const rowOne = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
+const rowTwo = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
+const rowThree = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'];
+const rowFour = ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'];
+const rowFive = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'];
+const rowSix = ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '◀', '▼', '▶', 'Ctrl'];
+
+
