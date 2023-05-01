bb8d3d02af42feb8c003e6e5a278a481169f7a5a
Krasavceva <krasavceva.va@gmail.com>
2023-05-01 22:44:30 +0300

    feat: create elements

--- index.js
+++ index.js
@@ -7,3 +7,42 @@ const rowFive = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲
 const rowSix = ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '◀', '▼', '▶', 'Ctrl'];
 
 
+// создание элементов страницы
+
+const header = document.createElement('header');
+header.className = 'header';
+header.innerHTML = 'RS Виртуальная клавиатура';
+document.body.append(header);
+
+const textarea = document.createElement('textarea');
+textarea.className = 'textarea';
+header.after(textarea);
+
+const keyboard = document.createElement('div');
+keyboard.className = 'keyboard';
+textarea.after(keyboard);
+
+const row1 = document.createElement('div');
+row1.className = 'row1';
+keyboard.prepend(row1);
+
+const row2 = document.createElement('div');
+row1.className = 'row2';
+row1.after(row2);
+
+const row3 = document.createElement('div');
+row1.className = 'row3';
+row2.after(row3);
+
+const row4 = document.createElement('div');
+row1.className = 'row4';
+row3.after(row4);
+
+const row5 = document.createElement('div');
+row1.className = 'row5';
+row4.after(row5);
+
+const footer = document.createElement('footer');
+footer.className = 'footer';
+footer.innerHTML = 'Клавиатура создана в операционной системе Windows<br>Для переключения языка комбинация: левые ctrl + alt';
+document.body.append(footer);
